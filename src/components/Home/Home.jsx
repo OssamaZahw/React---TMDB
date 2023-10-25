63e37ddfd64fa487351da277a94e0784ff5bd256
OssamaZahw <ossama.abouzahw@gmail.com>
2023-10-25 23:20:22 +0300

    search & filter improved

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,14 +9,17 @@ export default function Home() {
   const [apiUrl, setApiUrl] = useState(trendUrl);
   const [loading, error, apiArr] = useGetMovies(apiUrl);
   const [displayArr, setDisplayArr] = useState([]);
+  const [filterParam, setFilterParam] = useState('')
+  // let filterParam = 'movie';
 
 
   useEffect(() => {
     console.log("Api changed");
     if (!loading) {
-      setDisplayArr([...apiArr]);
+      filterMovies(filterParam);
+      // setDisplayArr([...apiArr]);
     }
-  }, [apiArr]);
+  }, [apiArr, filterParam]);
 
 
 
@@ -27,20 +30,32 @@ export default function Home() {
         setApiUrl(
           `https://api.themoviedb.org/3/search/multi?api_key=14bdd69ce887376edfafb09f23f78fe9&query=${searchParam}`
           );
+          
         } else {
-          setApiUrl(trendUrl)
+          setApiUrl(trendUrl);
+          // filterMovies(filterParam);
         }
   }
 
-  const handleFilter = (e) => {
-    if(e.target.value){
-      const filteredArr = apiArr.filter((el)=> el.media_type == e.target.value)
+  const filterMovies = (type) => {
+    if(type){
+      const filteredArr = apiArr.filter((el)=> el.media_type == type)
       console.log(filteredArr);
       setDisplayArr(filteredArr);
     } else {
       setDisplayArr(apiArr)
 
     }
+  }
+
+
+
+  const handleFilter = (e) => {
+    console.log(e.target.value)
+    setFilterParam(e.target.value)
+    // filterParam = e.target.value
+    console.log(filterParam)
+    filterMovies(filterParam);
   };
 
   return (
